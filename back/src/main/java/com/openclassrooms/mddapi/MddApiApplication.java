5471f6e8c2a5b28d5996672f6d1a517057730641
Mazoyer <mazoyeralexandre@hotmail.fr>
2024-02-09 15:09:59 +0100

    add env variable credentiel for argument

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -2,12 +2,8 @@ package com.openclassrooms.mddapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class MddApiApplication {
 
 	public static void main(String[] args) {
